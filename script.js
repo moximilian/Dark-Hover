1ebe276d9e787f206ebf4f52eaa4f7b5aa6e6ce7
Maxim Syrov <surovmax03@gmail.com>
2023-11-16 20:01:03 +0300

    lint fix

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-;(function () {
+; (function () {
     document.onmousemove = handleMouseMove
     function handleMouseMove(event) {
         var eventDoc, doc, body
-
-        event = event || window.event 
+        event = event || window.event
         if (event.pageX == null && event.clientX != null) {
             eventDoc = (event.target && event.target.ownerDocument) || document
             doc = eventDoc.documentElement
             body = eventDoc.body
-
             event.pageX =
                 event.clientX +
                 ((doc && doc.scrollLeft) || (body && body.scrollLeft) || 0) -
